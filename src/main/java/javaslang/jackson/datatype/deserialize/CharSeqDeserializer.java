be355703e9a5100959a770751721dd63a495b61e
Ruslan Sennov <ruslan.sennov@gmail.com>
2015-11-26 00:02:30 +0300

    CharSeqDeserializer should be Resolvable

--- src/main/java/javaslang/jackson/datatype/deserialize/CharSeqDeserializer.java
+++ src/main/java/javaslang/jackson/datatype/deserialize/CharSeqDeserializer.java
@@ -20,13 +20,15 @@ import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.deser.ResolvableDeserializer;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import javaslang.collection.CharSeq;
 
 import java.io.IOException;
 
-class CharSeqDeserializer extends StdDeserializer<CharSeq> {
+class CharSeqDeserializer extends StdDeserializer<CharSeq> implements ResolvableDeserializer {
 
     private static final long serialVersionUID = 1L;
 
@@ -38,14 +40,16 @@ class CharSeqDeserializer extends StdDeserializer<CharSeq> {
 
     @Override
     public CharSeq deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        if(deserializer == null) {
-            deserializer = ctxt.findRootValueDeserializer(TypeFactory.unknownType());
-        }
         Object obj = deserializer.deserialize(p, ctxt);
         if(obj instanceof String) {
             return CharSeq.of((String) obj);
         } else {
             throw ctxt.wrongTokenException(p, JsonToken.VALUE_STRING, "CharSeq can only be deserialized from String");
         }
+    }
+
+    @Override
+    public void resolve(DeserializationContext ctxt) throws JsonMappingException {
+        deserializer = ctxt.findContextualValueDeserializer(TypeFactory.unknownType(), null);
     }
 }
