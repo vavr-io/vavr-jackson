e30da3cea657871605c9fcd2637e248a78c61ff4
Ruslan Sennov <ruslan.sennov@gmail.com>
2015-11-27 07:58:29 +0300

    caching of KeyDeserializer

--- src/main/java/javaslang/jackson/datatype/deserialize/MapDeserializer.java
+++ src/main/java/javaslang/jackson/datatype/deserialize/MapDeserializer.java
@@ -22,6 +22,8 @@ import com.fasterxml.jackson.databind.deser.ResolvableDeserializer;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.type.MapLikeType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import javaslang.Tuple;
+import javaslang.Tuple2;
 import javaslang.collection.HashMap;
 import javaslang.collection.LinkedHashMap;
 import javaslang.collection.Map;
@@ -36,9 +38,8 @@ class MapDeserializer extends StdDeserializer<Map<?,?>> implements ResolvableDes
 
     private final JavaType javaType;
 
-    private JavaType keyType;
+    private Comparator<Object> keyComparator;
     private KeyDeserializer keyDeserializer;
-    private JavaType valueType;
     private JsonDeserializer<?> valueDeserializer;
 
     MapDeserializer(JavaType valueType) {
@@ -46,51 +47,42 @@ class MapDeserializer extends StdDeserializer<Map<?,?>> implements ResolvableDes
         this.javaType = valueType;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void resolve(DeserializationContext ctxt) throws JsonMappingException {
         MapLikeType type = mapLike(javaType, ctxt);
-        keyType = type.getKeyType();
+        JavaType keyType = type.getKeyType();
+        if(keyType.getRawClass().isAssignableFrom(Comparable.class)) {
+            keyComparator = (o1, o2) -> ((Comparable)o1).compareTo(o2);
+        } else {
+            keyComparator = (o1, o2) -> o1.toString().compareTo(o2.toString());
+        }
         keyDeserializer = ctxt.findKeyDeserializer(keyType, null);
-        valueType = type.getContentType();
-        valueDeserializer = ctxt.findContextualValueDeserializer(valueType, null);
+        valueDeserializer = ctxt.findContextualValueDeserializer(type.getContentType(), null);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public Map<?,?> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        final java.util.Map<Object, Object> result = new java.util.HashMap<>();
+        final java.util.List<Tuple2<Object, Object>> result = new java.util.ArrayList<>();
         while (p.nextToken() != JsonToken.END_OBJECT) {
             String name = p.getCurrentName();
             Object key = keyDeserializer.deserializeKey(name, ctxt);
             p.nextToken();
-            result.put(key, valueDeserializer.deserialize(p, ctxt));
+            result.add(Tuple.of(key, valueDeserializer.deserialize(p, ctxt)));
         }
-        if (TreeMap.class.isAssignableFrom(javaType.getRawClass())) {
-            Comparator<Object> c;
-            if(keyType.getRawClass().isAssignableFrom(Comparable.class)) {
-                c = (o1, o2) -> ((Comparable)o1).compareTo(o2);
-            } else {
-                c = (o1, o2) -> o1.toString().compareTo(o2.toString());
-            }
-            return fill(TreeMap.empty(c), result);
+        if (TreeMap.class.isAssignableFrom(handledType())) {
+            return TreeMap.ofAll(keyComparator, result);
         }
-        if (LinkedHashMap.class.isAssignableFrom(javaType.getRawClass())) {
-            return fill(LinkedHashMap.empty(), result);
+        if (LinkedHashMap.class.isAssignableFrom(handledType())) {
+            return LinkedHashMap.ofAll(result);
         }
         // default deserialization [...] -> Map
-        return fill(HashMap.empty(), result);
+        return HashMap.ofAll(result);
     }
 
     private static MapLikeType mapLike(JavaType type, DeserializationContext ctxt) {
         JavaType keyType = type.containedTypeCount() > 0 ? type.containedType(0) : TypeFactory.unknownType();
         JavaType valueType = type.containedTypeCount() > 1 ? type.containedType(1) : TypeFactory.unknownType();
         return ctxt.getTypeFactory().constructMapLikeType(type.getRawClass(), keyType, valueType);
-    }
-
-    private static javaslang.collection.Map<Object, Object> fill(javaslang.collection.Map<Object, Object> result, java.util.Map<Object, Object> content) {
-        for (java.util.Map.Entry<Object, Object> e : content.entrySet()) {
-            result = result.put(e.getKey(), e.getValue());
-        }
-        return result;
     }
 }
