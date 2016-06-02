831f02c3c0c79305beb52cb24ad14e4f679e8af1
Ruslan Sennov <ruslan.sennov@gmail.com>
2016-06-02 07:08:21 +0300

    EitherDeserializer refactoring

--- src/main/java/javaslang/jackson/datatype/deserialize/EitherDeserializer.java
+++ src/main/java/javaslang/jackson/datatype/deserialize/EitherDeserializer.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import javaslang.control.Either;
 
 import java.io.IOException;
@@ -43,9 +44,6 @@ class EitherDeserializer extends ValueDeserializer<Either<?, ?>> {
         boolean right = false;
         Object value = null;
         int cnt = 0;
-        if (stringDeserializer == null) {
-            stringDeserializer = ctxt.findContextualValueDeserializer(ctxt.constructType(String.class), null);
-        }
         while (p.nextToken() != JsonToken.END_ARRAY) {
             cnt++;
             switch (cnt) {
@@ -66,9 +64,10 @@ class EitherDeserializer extends ValueDeserializer<Either<?, ?>> {
                         value = deserializer(0).deserialize(p, ctxt);
                     }
                     break;
-                default:
-                    throw ctxt.mappingException(javaType.getRawClass());
             }
+        }
+        if (cnt != 2) {
+            throw ctxt.mappingException(javaType.getRawClass());
         }
         if(right) {
             return Either.right(value);
@@ -77,5 +76,9 @@ class EitherDeserializer extends ValueDeserializer<Either<?, ?>> {
         }
     }
 
-
+    @Override
+    public void resolve(DeserializationContext ctxt) throws JsonMappingException {
+        super.resolve(ctxt);
+        stringDeserializer = ctxt.findContextualValueDeserializer(ctxt.constructType(String.class), null);
+    }
 }
