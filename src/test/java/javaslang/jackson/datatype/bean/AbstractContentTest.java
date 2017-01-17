0f9b831310e356b689f42f9ce767a803f96b8848
Ruslan Sennov <ruslan.sennov@gmail.com>
2017-01-17 07:26:26 +0300

    + test

--- src/test/java/javaslang/jackson/datatype/bean/AbstractContentTest.java
+++ src/test/java/javaslang/jackson/datatype/bean/AbstractContentTest.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class AbstractContent {
+public class AbstractContentTest {
 
     public static class L {
         @JsonDeserialize(contentAs = X.class)
