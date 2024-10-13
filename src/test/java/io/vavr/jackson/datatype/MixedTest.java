7f0752fb0db47ed2e2058302e5d47c41ef1f9ce0
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-10-13 21:09:44 +0200

    MixedTest better names

--- src/test/java/io/vavr/jackson/datatype/MixedTest.java
+++ src/test/java/io/vavr/jackson/datatype/MixedTest.java
@@ -10,57 +10,59 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MixedTest extends BaseTest {
 
     @Test
-    void test1() throws IOException {
+    void shouldSerializeAndDeserializeVavrHashMapWithListValues() throws IOException {
         Object src = HashMap.empty().put("key1", List.of(1, 2)).put("key2", List.of(3, 4));
         String json = mapper().writer().writeValueAsString(src);
-        Assertions.assertEquals(mapper().readValue(json, new TypeReference<HashMap<?,List<?>>>() {}), src);
+        assertEquals(mapper().readValue(json, new TypeReference<HashMap<?, List<?>>>() {}), src);
     }
 
     @Test
-    void test2() throws IOException {
+    void shouldSerializeAndDeserializeListOfVavrHashMaps() throws IOException {
         Object src = List.of(HashMap.empty().put("key1", 1), HashMap.empty().put("key2", 2));
         String json = mapper().writer().writeValueAsString(src);
-        Assertions.assertEquals(mapper().readValue(json, new TypeReference<List<HashMap<?,?>>>() {}), src);
+        assertEquals(mapper().readValue(json, new TypeReference<List<HashMap<?, ?>>>() {}), src);
     }
 
     @Test
-    void test3() throws IOException {
+    void shouldSerializeAndDeserializeListContainingJavaMap() throws IOException {
         java.util.Map<String, String> javaHMap = new java.util.HashMap<>();
         javaHMap.put("1", "2");
         List<?> src = List.of(javaHMap);
         List<?> restored = mapper().readValue(mapper().writer().writeValueAsString(src), List.class);
-        Assertions.assertEquals(restored, src);
+        assertEquals(restored, src);
     }
 
     @Test
-    void test4() {
+    void shouldThrowJsonMappingExceptionForInvalidListElements() {
         Assertions.assertThrows(JsonMappingException.class, () -> {
             mapper().readValue("[\"s\"]", new TypeReference<List<Integer>>() {});
         });
     }
 
     @Test
-    void test5() throws IOException {
+    void shouldSerializeAndDeserializeNestedVavrLists() throws IOException {
         Object src = List.of(List.of(1));
         String json = mapper().writer().writeValueAsString(src);
-        Assertions.assertEquals(mapper().readValue(json, new TypeReference<List<List<?>>>() {}), src);
+        assertEquals(mapper().readValue(json, new TypeReference<List<List<?>>>() {}), src);
     }
 
     @Test
-    void test6() throws IOException {
+    void shouldSerializeAndDeserializeVavrHashMapWithNestedHashMapValues() throws IOException {
         Object src = HashMap.empty().put("1", HashMap.empty().put("X", "Y"));
         String json = mapper().writer().writeValueAsString(src);
-        Assertions.assertEquals(mapper().readValue(json, new TypeReference<HashMap<?, HashMap<?, ?>>>() {}), src);
+        assertEquals(mapper().readValue(json, new TypeReference<HashMap<?, HashMap<?, ?>>>() {}), src);
     }
 
     @Test
-    void test7() throws IOException {
+    void shouldSerializeAndDeserializeListContainingJavaList() throws IOException {
         Object src = List.of(Arrays.asList(1, 2));
         String json = mapper().writer().writeValueAsString(src);
-        Assertions.assertEquals(mapper().readValue(json, List.class), src);
-        Assertions.assertEquals(mapper().readValue(json, new TypeReference<List<java.util.List<?>>>() {}), src);
+        assertEquals(mapper().readValue(json, List.class), src);
+        assertEquals(mapper().readValue(json, new TypeReference<List<java.util.List<?>>>() {}), src);
     }
 }
