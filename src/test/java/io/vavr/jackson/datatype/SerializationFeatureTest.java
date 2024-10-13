6b63dbf6708750f3e7349d708531f507d10c1d84
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-10-13 21:01:30 +0200

    ScalarTest better names

--- src/test/java/io/vavr/jackson/datatype/SerializationFeatureTest.java
+++ src/test/java/io/vavr/jackson/datatype/SerializationFeatureTest.java
@@ -8,44 +8,35 @@ import com.fasterxml.jackson.datatype.joda.JodaModule;
 import io.vavr.collection.List;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SerializationFeatureTest {
 
-    private static CsvMapper getMapper() {
-        final CsvMapper csvMapper = new CsvMapper();
-        csvMapper.registerModule(new JodaModule());
-        csvMapper.registerModule(new VavrModule());
-        return csvMapper;
-    }
-
     @Test
-    public void vavr_List_DateTime_serialization_should_use_SerializationFeature() throws IOException {
-        final DateTime dateTime = new DateTime(2016, 6, 6, 8, 0, DateTimeZone.forID("CET"));
-        final io.vavr.collection.List<DateTime> dateTimeList = List.of(dateTime);
-        final java.util.List<DateTime> dateTimeJavaList = new ArrayList<>();
+    public void shouldApplySerializationFeatureListDateTime() throws IOException {
+        DateTime dateTime = new DateTime(2016, 6, 6, 8, 0, DateTimeZone.forID("CET"));
+        io.vavr.collection.List<DateTime> dateTimeList = List.of(dateTime);
+        java.util.List<DateTime> dateTimeJavaList = new ArrayList<>();
         dateTimeJavaList.add(dateTime);
 
-        final CsvMapper mapper = getMapper();
-        final ObjectWriter writer = mapper.writer()
-                .without(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        CsvMapper mapper = new CsvMapper();
+        mapper.registerModule(new JodaModule());
+        mapper.registerModule(new VavrModule());
+        ObjectWriter writer = mapper.writer().without(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        final String serializedDateTime = writer
-                .writeValueAsString(dateTime);
-        final String serializedDateTimeJavaList = writer
-                .writeValueAsString(dateTimeJavaList);
-        final String serializedDateTimeList = writer
-                .writeValueAsString(dateTimeList);
+        String serializedDateTime = writer.writeValueAsString(dateTime);
+        String serializedDateTimeJavaList = writer.writeValueAsString(dateTimeJavaList);
+        String serializedDateTimeList = writer.writeValueAsString(dateTimeList);
 
-        Assertions.assertEquals(serializedDateTime, serializedDateTimeJavaList);
-        Assertions.assertEquals(serializedDateTimeJavaList, serializedDateTimeList);
+        assertEquals(serializedDateTime, serializedDateTimeJavaList);
+        assertEquals(serializedDateTimeJavaList, serializedDateTimeList);
 
         List<DateTime> restored = mapper.readValue(serializedDateTimeList, new TypeReference<List<DateTime>>() {});
-        Assertions.assertEquals(restored.head().getMillis(), dateTime.getMillis());
-
+        assertEquals(restored.head().getMillis(), dateTime.getMillis());
     }
 }
--- src/test/java/io/vavr/jackson/datatype/ServiceLoaderTest.java
+++ src/test/java/io/vavr/jackson/datatype/ServiceLoaderTest.java
@@ -1,6 +1,5 @@
 package io.vavr.jackson.datatype;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vavr.Lazy;
 import org.junit.jupiter.api.Assertions;
@@ -8,20 +7,21 @@ import org.junit.jupiter.api.Test;
 
 public class ServiceLoaderTest {
 
-  /**
-   * Since there is no way (without reflection) to interrogate the mapper to see which modules
-   * are loaded, we can do so indirectly by simply asking it to handle any vavr type.
-   * Non-failure means success
-   * @throws Exception only if jackson fails to handle vavr types - meaning auto-register didn't work
-   */
-  @Test
-  void testAutoDiscovery() throws Exception {
-    ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
+    /**
+     * Since there is no way (without reflection) to interrogate the mapper to see which modules
+     * are loaded, we can do so indirectly by simply asking it to handle any vavr type.
+     * Non-failure means success
+     *
+     * @throws Exception only if jackson fails to handle vavr types - meaning auto-register didn't work
+     */
+    @Test
+    void shouldAutoDiscover() throws Exception {
+        ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
 
-    Lazy<?> src = Lazy.of(() -> 1);
-    String json = mapper.writer().writeValueAsString(src);
-    Assertions.assertEquals("1", json);
-    Lazy<?> restored = mapper.readValue(json, Lazy.class);
-    Assertions.assertEquals(src, restored);
-  }
+        Lazy<?> src = Lazy.of(() -> 1);
+        String json = mapper.writer().writeValueAsString(src);
+        Assertions.assertEquals("1", json);
+        Lazy<?> restored = mapper.readValue(json, Lazy.class);
+        Assertions.assertEquals(src, restored);
+    }
 }
