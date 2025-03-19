1833e4fd625ec7b8262f745f0afca41a6197e3ed
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2025-03-19 21:20:28 +0100

    Map support for @JsonMerge

---
+++ src/test/java/io/vavr/jackson/datatype/map/HashMapJsonMergeTest.java
@@ -0,0 +1,46 @@
+package io.vavr.jackson.datatype.map;
+
+import com.fasterxml.jackson.annotation.JsonMerge;
+import com.fasterxml.jackson.annotation.OptBoolean;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.vavr.Tuple;
+import io.vavr.collection.HashMap;
+import io.vavr.jackson.datatype.BaseTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HashMapJsonMergeTest extends BaseTest {
+
+    static class TestJsonMergeWithHashMap {
+        @JsonMerge(OptBoolean.TRUE)
+        public HashMap<String, String> value = HashMap.of("foo1", "bar1");
+    }
+
+    static class TestJsonMergeWithNestedMap {
+        @JsonMerge(OptBoolean.TRUE)
+        public HashMap<String, HashMap<String, String>> value = HashMap.of("foo1", HashMap.of("foo_nested", "bar_nested"));
+    }
+
+    @Test
+    public void shouldMergeString() throws Exception {
+        TestJsonMergeWithHashMap result = mapper().readValue(asJson("{'value': {'foo2':'bar2'}}"), TestJsonMergeWithHashMap.class);
+
+        assertThat(result.value)
+            .hasSize(2)
+            .containsExactly(
+                Tuple.of("foo1", "bar1"),
+                Tuple.of("foo2", "bar2"));
+    }
+
+    @Test
+    public void shouldMergeNested() throws JsonProcessingException {
+        TestJsonMergeWithNestedMap result = mapper().readValue(asJson("{'value': {'foo1': {'foo_nested': 'bar_nested', 'foo_nested2': 'bar_nested2'}}}"), TestJsonMergeWithNestedMap.class);
+
+        assertThat(result.value)
+            .hasSize(1)
+            .containsExactly(Tuple.of("foo1", HashMap.of(
+                "foo_nested", "bar_nested",
+                "foo_nested2", "bar_nested2")));
+    }
+}
